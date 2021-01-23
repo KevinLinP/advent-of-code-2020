1e08209f652cb9a1b1ad130055a501e2f5c1e850
Kevin Lin <kevin@Kevins-MacBook-Air.local>
2021-01-23 11:36:15 -0500

    1: finish part 1

--- 1.rs
+++ 1.rs
@@ -1,21 +1,27 @@
 use std::fs::File;
-use std::io::{self, prelude::*, BufReader};
+use std::io::{prelude::*, BufReader};
 use std::collections::HashSet;
 
-fn main() -> io::Result<()> {
+fn main() {
     let numbers = parse_input();
 
     for number in &numbers {
-        println!("{}", number);
-    }
+        if number > &1010i32 {
+            continue;
+        }
 
-    Ok(())
+        let other_number = 2020 - number;
+        if numbers.contains(&other_number) {
+            println!("{}", number * other_number);
+            return
+        }
+    }
 }
 
 fn parse_input() -> HashSet<i32> {
     let mut numbers = HashSet::new();
 
-    let file = File::open("1.input.sample").unwrap();
+    let file = File::open("1.input").unwrap();
     let reader = BufReader::new(file);
 
     for line in reader.lines() {
