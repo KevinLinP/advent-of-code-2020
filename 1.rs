06428f55bb997e8f15e8c1b7002353df8938910e
Kevin Lin <kevin@Kevins-MacBook-Air.local>
2021-01-23 12:32:43 -0500

    1: finish part 2

--- 1.rs
+++ 1.rs
@@ -3,10 +3,32 @@ use std::io::{prelude::*, BufReader};
 use std::collections::HashSet;
 
 fn main() {
-    let numbers = parse_input();
+    let numbers_set = parse_input();
+    //part_1(numbers_set);
+    let mut numbers = Vec::new();
 
+    for number in &numbers_set {
+        numbers.push(number);
+    }
+
+    for (first_index, first_number_ref) in numbers.iter().enumerate() {
+        let first_number = *first_number_ref;
+        for second_index in first_index..(numbers_set.len() - 1) {
+            let second_number = numbers[second_index];
+            let third_number = 2020i32 - first_number - second_number;
+
+            if numbers_set.contains(&third_number) {
+                println!("{}", first_number * second_number * third_number);
+                return
+            }
+        }
+    }
+}
+
+#[allow(dead_code)]
+fn part_1(numbers: HashSet<i32>) {
     for number in &numbers {
-        if number > &1010i32 {
+        if *number > 1010i32 {
             continue;
         }
 
