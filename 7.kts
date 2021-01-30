c80561e03f9c009b740eebf737899d7304f2d2c8
Kevin Lin <kevin.lin.p@gmail.com>
2021-01-30 14:51:56 -0500

    7: part 1

--- 7.kts
+++ 7.kts
@@ -1,21 +1,63 @@
 import java.io.File
+import java.util.LinkedList
 
 // to run:
 // kotlinc -script 7.kts
 
-
 class Solver() {
-    val map = HashMap<String, MutableSet<String>>()
+    val containees = HashMap<String, MutableSet<String>>()
 
-    val lineRegex = Regex("""(.+)\scontain\s(.+).""")
-    val rightRegex = Regex("""(\d)\s(.+)""")
+    val lineRegex = Regex("""(.+) bags contain (.+).""")
+    val rightRegex = Regex("""(\d+) (.+) bags?""")
 
     fun solve() {
         parseInput()
+        printContainees()
+        printDescendants("shiny gold")
     }
 
-    fun parseInput() {
-        File("7.input.sample").forEachLine {
+    private fun printDescendants(start: String) {
+        val descendants = HashSet<String>()
+        val queue = LinkedList<String>()
+        queue.addLast(start)
+
+        while (queue.size > 0) {
+            val current = queue.removeFirst()
+            val containers = containees[current]
+
+            if (containers != null) {
+                for (container in containers) {
+                    if (!descendants.contains(container)) {
+                        descendants.add(container)
+                        queue.addLast(container)
+                    }
+                }
+            }
+        }
+
+        println("descendants:")
+        for (descendant in descendants) {
+            println(descendant)
+        }
+        println(descendants.size)
+    }
+
+    private fun printContainees() {
+        println("containees:")
+        for (containee in containees.keys) {
+            println("'$containee'")
+            val containers = containees[containee]
+            if (containers != null) {
+                for (container in containers) {
+                    println("'$container'")
+                }
+            }
+            println()
+        }
+    }
+
+    private fun parseInput() {
+        File("7.input").forEachLine {
             val matchResult = lineRegex.find(it)
             val groups = matchResult!!.groups
 
@@ -27,23 +69,24 @@ class Solver() {
     }
 
     // TODO: holy shit come up with better names for this crap
-    fun parseLine(left: String, rightArray: Collection<String>) {
+    private fun parseLine(left: String, rightArray: Collection<String>) {
         println(left)
 
         for(right in rightArray) {
+            //println(right)
             val matchResult = rightRegex.find(right)
             if (matchResult == null) { continue } // contain no other bags case
 
             val rightType = matchResult.groups.get(2)!!.value
             println(rightType)
 
-            var parentSet = map[right]
-            if (parentSet == null) {
-                parentSet = HashSet<String>()
-                parentSet.add(left)
-                map[rightType] = parentSet
+            var containers = containees[rightType]
+            if (containers == null) {
+                containers = HashSet<String>()
+                containers.add(left)
+                containees[rightType] = containers
             } else {
-                parentSet.add(left)
+                containers.add(left)
             }
         }
 
