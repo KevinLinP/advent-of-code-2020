dfc05105b461e14c009af20ad8fea4e9dcefd168
Kevin Lin <kevin.lin.p@gmail.com>
2021-01-29 22:47:32 -0500

    6: part 2

--- 6.ts
+++ 6.ts
@@ -1,5 +1,5 @@
 import "wasi";
-import { Console, FileSystem } from "as-wasi";
+import { FileSystem } from "as-wasi";
 
 // compile and run:
 // asc 6.ts -b 6.wasm --runtime minimal && wasmtime --dir=. 6.wasm
@@ -15,29 +15,54 @@ const solve = function (): void {
 
   const lines = input.split("\n")
 
-  let count = 0
-  let current = new Set<String>()
+  let anyTotal = 0
+  let allTotal = 0
+
+  // {} as an empty object doesn't exist either
+  let any = new Set<String>()
+  let all = new Map<String, i32>()
+  let personCount = 0
 
   // assemblyscript hasn't implemented closures nor iterator =(
   for (let i = 0; i < lines.length; i++) {
     const line = lines[i]
 
-    if (line === '') {
-      let currentCount = current.size
-      console.log(currentCount.toString())
-      count += currentCount
-      current.clear()
-    } else {
+    if (line !== '') {
+      personCount += 1
+
       const chars = line.split('')
       for (let j = 0; j < chars.length; j++) {
         const char = chars[j]
-        current.add(char)
+
+        any.add(char)
+
+        const charCount = all.has(char) ? all.get(char) : 0
+        all.set(char, charCount + 1)
       }
+    } else {
+      let anyCount = any.size
+      console.log(anyCount.toString())
+      anyTotal += anyCount
+      any.clear()
+
+      let allCount = 0
+      const allValues = all.values()
+      for (let j = 0; j < allValues.length; j++) {
+        if (allValues[j] === personCount) {
+          allCount += 1
+        }
+      }
+      console.log(allCount.toString())
+      allTotal += allCount
+      all.clear()
+
+      personCount = 0
     }
-    Console.log(line)
+    console.log(line)
   }
 
-  console.log(count.toString())
+  console.log(anyTotal.toString())
+  console.log(allTotal.toString())
 }
 
 solve()
