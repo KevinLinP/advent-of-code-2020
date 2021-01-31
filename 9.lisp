9651125f46da82b8ca11b694f40bce986bd58f6f
Kevin Lin <kevin.lin.p@gmail.com>
2021-01-31 00:49:54 -0500

    9: suffering

--- 9.lisp
+++ 9.lisp
@@ -1,5 +1,7 @@
 (load "~/quicklisp/setup.lisp")
 (ql:quickload "alexandria")
+(ql:quickload "iterate")
+(use-package :iterate)
 
 (defparameter preamble 5)
 
@@ -16,18 +18,36 @@
 
 (defparameter *current-numbers* (make-hash-table))
 
-(defparameter lower-index-start (+ (* -1 preamble) 1))
-(defparameter index-to-remove-start (* -1 preamble))
-(loop for num across numbers
-      for upper-index from -1
-      for lower-index from (- 0 preamble)
-      for index-to-remove from (- 0 preamble 1)
-      do (setf (gethash num *current-numbers*) T)
-      if (>= index-to-remove 0)
-      do (remhash (aref numbers index-to-remove) *current-numbers*)
-      if (>= lower-index 0)
-      do (loop
-           for current-index from lower-index
-           while (<= current-index upper-index)
-           do (format t "~d~%" current-index))
-      do (format t "~%"))
+(iterate
+  (for target in-vector numbers)
+  (for target-index from 0)
+  (for upper-index from -1)
+  (for lower-index from (- 0 preamble))
+  (for index-to-remove from (- 0 preamble 1))
+  ;(format t "~d ~d ~d ~d ~d ~%" target target-index upper-index lower-index index-to-remove)
+  (if (>= upper-index 0)
+    (setf (gethash (aref numbers upper-index) *current-numbers*) T))
+  ;(if (>= index-to-remove 0)
+    ;(remhash (aref numbers index-to-remove) *current-numbers*)
+  (if (< lower-index 0) (next-iteration))
+  (iterate
+    (for current-index from lower-index to upper-index)
+    (format t "~d " (aref numbers current-index)))
+  (format t "~%")
+  )
+
+;(defparameter lower-index-start (+ (* -1 preamble) 1))
+;(defparameter index-to-remove-start (* -1 preamble))
+;(loop for num across numbers
+      ;for upper-index from -1
+      ;for lower-index from (- 0 preamble)
+      ;for index-to-remove from (- 0 preamble 1)
+      ;do (setf (gethash num *current-numbers*) T)
+      ;if (>= index-to-remove 0)
+      ;do (remhash (aref numbers index-to-remove) *current-numbers*)
+      ;if (>= lower-index 0)
+      ;do (loop
+           ;for current-index from lower-index
+           ;while (and (<= current-index upper-index) ())
+           ;do (format t "~d~%" current-index))
+      ;do (format t "~%"))
