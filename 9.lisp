ca40bd0602805d0e40f4113861d1df00ca45a810
Kevin Lin <kevin.lin.p@gmail.com>
2021-01-31 01:18:26 -0500

    9: part 1, kinda.

--- 9.lisp
+++ 9.lisp
@@ -3,10 +3,10 @@
 (ql:quickload "iterate")
 (use-package :iterate)
 
-(defparameter preamble 5)
+(defparameter preamble 25)
 
 (defparameter number-list
-  (with-open-file (input "9.input.sample")
+  (with-open-file (input "9.input")
     (loop for line = (read-line input nil)
           while line
           collect (parse-integer line))))
@@ -16,38 +16,30 @@
     (list (length number-list))
     :initial-contents number-list))
 
-(defparameter *current-numbers* (make-hash-table))
+;(defparameter *current-numbers* (make-hash-table))
 
 (iterate
   (for target in-vector numbers)
   (for target-index from 0)
   (for upper-index from -1)
   (for lower-index from (- 0 preamble))
-  (for index-to-remove from (- 0 preamble 1))
+  ;(for index-to-remove from (- 0 preamble 1))
   ;(format t "~d ~d ~d ~d ~d ~%" target target-index upper-index lower-index index-to-remove)
-  (if (>= upper-index 0)
-    (setf (gethash (aref numbers upper-index) *current-numbers*) T))
+  ;(if (>= upper-index 0)
+    ;(setf (gethash (aref numbers upper-index) *current-numbers*) T))
   ;(if (>= index-to-remove 0)
     ;(remhash (aref numbers index-to-remove) *current-numbers*)
   (if (< lower-index 0) (next-iteration))
+  (format t "~d: " target)
   (iterate
-    (for current-index from lower-index to upper-index)
-    (format t "~d " (aref numbers current-index)))
+    (for left-index from lower-index to (- upper-index 1))
+    (for left-number = (aref numbers left-index))
+    (iterate
+      (for right-index from (+ left-index 1) to upper-index)
+      (for right-number = (aref numbers right-index))
+      (for total = (+ left-number right-number))
+      (if (eq target total) (format t "~d ~d " left-number right-number))
+      )
+    )
   (format t "~%")
   )
-
-;(defparameter lower-index-start (+ (* -1 preamble) 1))
-;(defparameter index-to-remove-start (* -1 preamble))
-;(loop for num across numbers
-      ;for upper-index from -1
-      ;for lower-index from (- 0 preamble)
-      ;for index-to-remove from (- 0 preamble 1)
-      ;do (setf (gethash num *current-numbers*) T)
-      ;if (>= index-to-remove 0)
-      ;do (remhash (aref numbers index-to-remove) *current-numbers*)
-      ;if (>= lower-index 0)
-      ;do (loop
-           ;for current-index from lower-index
-           ;while (and (<= current-index upper-index) ())
-           ;do (format t "~d~%" current-index))
-      ;do (format t "~%"))
