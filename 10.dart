a09745a7fcab33129fcf17bf263352b9e34225ff
Kevin Lin <kevin.lin.p@gmail.com>
2021-02-06 12:08:29 -0500

    10: part 2

--- 10.dart
+++ 10.dart
@@ -2,21 +2,51 @@ import 'dart:convert';
 import 'dart:io';
 
 void main() async {
-  final counts = new Map<num, num>();
-
   var numbers = await parseInput();
   numbers.sort();
   print(numbers);
+
+  onesTimesThrees(numbers);
+  print('');
+
+  numPaths(numbers + [numbers.last + 3]);
+}
+
+// given answer > trillions,
+// this requires int to be 64-bits, which it should be
+void numPaths(List<int> numbers) {
+  final allNumPaths = new Map<int, int>();
+  allNumPaths[0] = 1;
+
+  for (var number in numbers) {
+    int numPaths = 0;
+
+    for (var offset in [-1, -2, -3]) {
+      int prevNum = number + offset; // subtract the offset at your own peril
+      var prevNumPaths = allNumPaths[prevNum];
+      if (prevNumPaths != null) {
+        numPaths += prevNumPaths;
+      }
+    }
+    allNumPaths[number] = numPaths;
+  }
+
+  print(allNumPaths);
+}
+
+void onesTimesThrees(List<int> numbers) {
+  final counts = new Map<int, int>();
 
   var previousNumber = 0;
   for (int i = 0; i < numbers.length; i++) {
     final number = numbers[i];
     final difference = number - previousNumber;
 
-    if (counts.containsKey(difference)) {
-      counts[difference] += 1;
-    } else {
+    var previousDifference = counts[difference];
+    if (previousDifference == null) {
       counts[difference] = 1;
+    } else {
+      counts[difference] = previousDifference + 1;
     }
 
     previousNumber = number;
@@ -29,8 +59,8 @@ void main() async {
   print('${counts[1]} * ${counts[3]} = ${counts[1] * counts[3]}');
 }
 
-Future<List<num>> parseInput() async {
-  List<num> numbers = [];
+Future<List<int>> parseInput() async {
+  List<int> numbers = [];
 
   Stream<List<int>> stream = new File('10.input').openRead();
 
