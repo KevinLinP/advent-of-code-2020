43d0d929d8c1339af2be9239c8f9a0f3b42fd857
Kevin Lin <kevin.lin.p@gmail.com>
2021-02-06 20:04:56 -0500

    11: part 2, start

--- 11.jl
+++ 11.jl
@@ -1,12 +1,23 @@
 import Printf.@printf
 
-function parseinput(inputpath)
+struct InputSpec
+  path
+  width
+  height
+end
+
+struct CoordWithVis
+  coord::CartesianIndex
+  visiblecoords::Vector{CartesianIndex}
+end
+
+function parseinput(inputpath, offset)
   coords = Vector{CartesianIndex}()
 
   for (y, line) in enumerate(eachline(inputpath))
     for (x, char) in enumerate(line)
       if char == 'L'
-        push!(coords, CartesianIndex(y+1, x+1))
+        push!(coords, CartesianIndex(y+offset, x+offset))
       end
     end
   end
@@ -14,26 +25,76 @@ function parseinput(inputpath)
   return coords
 end
 
-function printgrid(grid)
-  # TODO: make better
-  @show grid
+function calcdirections()
+  directions = collect(Base.product(-1:1, -1:1))
+  directions = [(directions)...]
+  directions = filter(x -> (x[1] != 0 || x[2] != 0), directions)
+
+  @show directions
+
+  return directions
+end
+
+function calculate_coords_with_visible_coords(inputspec)
+  coords = parseinput(inputspec.path, 0)
+  grid = falses(inputspec.height, inputspec.width)
+  directions = calcdirections()
+
+  for coord in coords
+    grid[coord] = true
+  end
+
+  coords_with_vis = Vector{CoordWithVis}()
+
+  for coord in coords
+    visiblecoords = Vector{CartesianIndex}()
+
+    for direction in directions
+      x = coord[2] + direction[1]
+      y = coord[1] + direction[2]
+
+      while (x >= 1) && (x <= inputspec.width) && (y >= 1) && (y <= inputspec.height)
+        if grid[y, x]
+          push!(visiblecoords, CartesianIndex(y, x))
+          break
+        else
+          x += direction[1]
+          y += direction[2]
+        end
+      end
+    end
+
+    push!(coords_with_vis, CoordWithVis(coord, visiblecoords))
+  end
+
+  @show coords_with_vis
+
+  return coords_with_vis
+end
+
+
+function solvevisible()
+  inputspec = InputSpec("11.input.sample", 10, 10)
+
+  coords_with_visible_coords = calculate_coords_with_visible_coords(inputspec)
 end
 
 # i'm oversizing the array by +2 in each direction
-# to avoid doing conditional checks inside the loop
-function solve()
-  # inputspec = ("10.input.sample", 10, 10)
+# to avoid doing conditional checks
+function solveneighbor()
+  # inputspec = ("11.input.sample", 10, 10)
   inputspec = ("11.input", 92, 94)
   xsize = inputspec[2] + 2 # Julia arrays start at 1
   ysize = inputspec[3] + 2
 
   currentgrid = falses(ysize, xsize)
   nextgrid = falses(ysize, xsize)
-  coords = parseinput(inputspec[1])
+  coords = parseinput(inputspec[1], 1)
 
   anychanged = true
-
+  iterationcount = 0
   while anychanged
+    iterationcount += 1
     anychanged = false
 
     for coord in coords
@@ -65,9 +126,15 @@ function solve()
     # nextgrid = tempgrid
   end
 
-  @printf("%i\r\n", count(b->(b), nextgrid))
+  @printf("iterationcount: %i, filledseats: %i\r\n", iterationcount, count(b->(b), nextgrid))
+end
+
+function printgrid(grid)
+  # TODO: make better
+  @show grid
 end
 
 @time begin
-  solve()
+  # solveneighbor()
+  solvevisible()
 end
