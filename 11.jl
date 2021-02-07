cf03ca9efadf12c40e8e134a90848f5423edfc7a
Kevin Lin <kevin.lin.p@gmail.com>
2021-02-06 20:20:51 -0500

    11: part 2

--- 11.jl
+++ 11.jl
@@ -30,7 +30,7 @@ function calcdirections()
   directions = [(directions)...]
   directions = filter(x -> (x[1] != 0 || x[2] != 0), directions)
 
-  @show directions
+  # @show directions
 
   return directions
 end
@@ -67,16 +67,52 @@ function calculate_coords_with_visible_coords(inputspec)
     push!(coords_with_vis, CoordWithVis(coord, visiblecoords))
   end
 
-  @show coords_with_vis
+  # @show coords_with_vis
 
   return coords_with_vis
 end
 
 
 function solvevisible()
-  inputspec = InputSpec("11.input.sample", 10, 10)
+  # inputspec = InputSpec("11.input.sample", 10, 10)
+  inputspec = InputSpec("11.input", 92, 94)
 
-  coords_with_visible_coords = calculate_coords_with_visible_coords(inputspec)
+  currentgrid = falses(inputspec.height, inputspec.width)
+  nextgrid = falses(inputspec.height, inputspec.width)
+
+  allcoordwithvis = calculate_coords_with_visible_coords(inputspec)
+
+  anychanged = true
+  iterationcount = 0
+  while anychanged
+    anychanged = false
+    iterationcount += 1
+
+    for coordwithvis in allcoordwithvis
+      coord = coordwithvis.coord
+      currentvalue = currentgrid[coord]
+      occupiedcount = 0
+
+      # could early exit on >0 and >=5
+      for visiblecoord in coordwithvis.visiblecoords
+        if currentgrid[visiblecoord]
+          occupiedcount += 1
+        end
+      end
+
+      if currentvalue && occupiedcount >= 5
+        nextgrid[coord] = false
+        anychanged = true
+      elseif !currentvalue && occupiedcount == 0
+        nextgrid[coord] = true
+        anychanged = true
+      end
+    end
+
+    copyto!(currentgrid, nextgrid)
+  end
+
+  @printf("iterationcount: %i, filledseats: %i\r\n", iterationcount, count(b->(b), currentgrid))
 end
 
 # i'm oversizing the array by +2 in each direction
@@ -94,8 +130,8 @@ function solveneighbor()
   anychanged = true
   iterationcount = 0
   while anychanged
-    iterationcount += 1
     anychanged = false
+    iterationcount += 1
 
     for coord in coords
       # @show coord
