157c6b55ca11748ae1fcc7fe085dd432a06905a5
Kevin Lin <kevin.lin.p@gmail.com>
2021-02-07 17:43:29 -0500

    15: start part 2

--- 12.rs
+++ 12.rs
@@ -1,21 +1,23 @@
-const TARGET: usize = 2020;
-const START: [u16; 6] = [10,16,6,0,1,17];
+use fnv::FnvHashMap;
+
+const TARGET: usize = 30000000;
+const START: [u32; 3] = [0,3,6];
 
 fn main() {
-    let mut all_last_mentioned = [0u16; TARGET];
+    let mut all_last_mentioned = [0u32; TARGET / 16];
 
     for (turn_num, num) in START.iter().enumerate() {
-        all_last_mentioned[*num as usize] = turn_num as u16 + 1;
+        all_last_mentioned[*num as usize] = turn_num as u32 + 1;
         //println!("{}", num);
     }
 
-    //for (i, num) in all_last_mentioned.iter().enumerate() {
-        //print!("{}:{}, ", i, num);
-    //}
-    //println!("");
+    for (i, num) in all_last_mentioned.iter().enumerate() {
+        print!("{}:{}, ", i, num);
+    }
+    println!("");
 
-    let mut current_num = 0u16;
-    let mut last_number_mentioned_ago = 0u16;
+    let mut current_num = 0u32;
+    let mut last_number_mentioned_ago = 0u32;
 
     for iteration in (START.len() + 1)..(TARGET+1) {
         if last_number_mentioned_ago == 0 {
@@ -29,10 +31,10 @@ fn main() {
         if current_num_last_mentioned == 0 {
             last_number_mentioned_ago = 0;
         } else {
-            last_number_mentioned_ago = iteration as u16 - current_num_last_mentioned;
+            last_number_mentioned_ago = iteration as u32 - current_num_last_mentioned;
         }
 
-        all_last_mentioned[current_num as usize] = iteration as u16;
+        all_last_mentioned[current_num as usize] = iteration as u32;
     }
 
     println!("{}", current_num);
